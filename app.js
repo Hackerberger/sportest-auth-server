1cccbd89393e10b5cc95c85525de01ce7347fedf
Hackerberger <hechenberger.l00@htl-ottakring.ac.at>
2020-04-15 10:15:34 +0200

    richtige files eingefügt

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 
 var app = express();
@@ -14,12 +13,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.static('public'));
+//app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
 module.exports = app;
